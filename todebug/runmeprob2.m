c8ce61739dd44bcd03f4daafce32a46c8f71118f
UniPD-DII-ETCOMP <55701889+UniPD-DII-ETCOMP@users.noreply.github.com>
2019-11-04 09:40:31 +0100

    Update runmeprob2.m

--- todebug/runmeprob2.m
+++ todebug/runmeprob2.m
@@ -1,1 +1,15 @@
 
+ 
+%hodlroption('block-size',100);
+%hodlroption('compression','svd');
+%hodlroption('threshold',1e-6);
+
+% Rhodlr=hodlr(R2); 
+% Lhodlr=hodlr('handle',fun_handlejwL,N.cotree,N.cotree);
+
+load prob2.mat
+
+disp('sum')
+tic
+SYSh=Rhodlr+Lhodlr;
+toc
